5575ea5dcbd0ae574e2eef5ff8ecb828f9f2aa04
junyeong <wn8624@naver.com>
2022-02-15 14:03:58 +0900

    0215

--- 백준/0214/숫자의 개수.c
+++ 백준/0214/숫자의 개수.c
@@ -5,20 +5,21 @@ int main()
     int result = 1,  input, index;
     int num[10];
 
+    for  (int i = 0; i < 10; i++)    num[i] = 0;  
 
-    for  (int i = 0; i < 3; i++)
+    for  (int i = 0; i < 3; i++) 
     {
         scanf("%d", &input);
         result *= input;
     }
 
-    for (int i=0; result>0; i++)
+    for (int i=0; result>0; i++) 
     {
-        index = result % 10;
-        num[index] += 1;
+        index = result % 10; 
+        num[index] += 1; 
         result /= 10;
     }
     
-    for (int i=0; i<10; i++) printf("%d\n", num[i]);
+    for (int i=0; i<10; i++) printf("%d\n", num[i]); 
     return (0);
 }
--- 백준/0214/최댓값.c
+++ 백준/0214/최댓값.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    int max = 0, arr[9], index;
-    for (int i=0; i<9; i++)
+    int max = 0, arr[9], index; //max값 0으로 초기화 
+    for (int i=0; i<9; i++) //9번 반복문
     {
-        scanf("%d", &arr[i]);
-        if  (max < arr[i])
+        scanf("%d", &arr[i]); //9번동안 배열에 입력값 받음
+        if  (max < arr[i]) //받을때마다 max보다 받은 배열의 값이 크면 
         {
-            max = arr[i];
-            index = i;
+            max = arr[i]; //max에는 받은 배열의 값중 제일 큰 값이 들어감 
+            index = i; //index라는 변수에 해당하는 수의 인덱스값이 들어감 
         }
     }
-    printf("%d\n%d", max, index + 1);
+    printf("%d\n%d", max, index + 1); 
     return (0);
 }
